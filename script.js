bdd257898bce4e08ecdeb74f899cde7cd16393a7
Yash12Kumar <yashkoli7211@gmail.com>
2026-09-26 16:53:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -55,6 +55,24 @@ document.getElementById("projectForm")?.addEventListener("submit", e => {
     data.get("details")
   ].join("\n");
 
-  window.location.href =
-    `mailto:${YOUR_EMAIL}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+  data.append("access_key", "42319a4b-ac92-4b86-a939-06f2e27368ca");
+
+fetch("https://api.web3forms.com/submit", {
+  method: "POST",
+  body: data
+})
+  .then(response => response.json())
+  .then(result => {
+    if (result.success) {
+      alert("Message sent successfully!");
+      document.getElementById("projectForm").reset();
+    } else {
+      alert("Message could not be sent.");
+      console.log(result);
+    }
+  })
+  .catch(error => {
+    console.log(error);
+    alert("Something went wrong.");
+  });
 });
